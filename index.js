ef651b4d38017331a9b334e71ca28bed0e197cdd
Stian Didriksen <stipsan@gmail.com>
2017-06-11 12:33:19 +0200

    Adding the font stack source, about to add prepack

---
+++ index.js
@@ -0,0 +1,15 @@
+// https://css-tricks.com/snippets/css/system-font-stack/
+
+module.exports = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  'Oxygen',
+  'Ubuntu',
+  'Cantarell',
+  '"Fira Sans"',
+  '"Droid Sans"',
+  '"Helvetica Neue"',
+  'sans-serif',
+].join(',')
