1b85f44284c167fd1fe07ed0ec56c599023d7ccd
Stian Didriksen <stipsan@gmail.com>
2017-11-05 03:32:32 +0100

    totally overkill

--- index.ts
+++ index.ts
@@ -1,16 +1,4 @@
 // Ensure the text got the same font as overall UI from user OS
 // https://css-tricks.com/snippets/css/system-font-stack/
 
-export default [
-  '-apple-system',
-  'BlinkMacSystemFont',
-  'Segoe UI',
-  'Roboto',
-  'Oxygen',
-  'Ubuntu',
-  'Cantarell',
-  'Fira Sans',
-  'Droid Sans',
-  'Helvetica Neue',
-  'sans-serif',
-].join(', ')
+export default '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif'
