cb78658c947fdc402f61aed4105fa6f3e8f2fcad
Stian Didriksen <stipsan@gmail.com>
2018-02-19 21:44:27 +0100

    Learning from github

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 // Ensure the text got the same font as overall UI from user OS
 // https://css-tricks.com/snippets/css/system-font-stack/
 
-export default '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif'
+export default '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
